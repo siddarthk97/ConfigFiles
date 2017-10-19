9c777a032337cab1e5f4a7449c4e5b3657c653fe
K Siddarth <siddarth@iitk.ac.in>
2017-10-19 19:00:28 +0530

    Added music player to i3bar

---
+++ src/battery.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DANGER=25
+SLEEP=2
+
+while true; do
+    BAT_PERCENT=$(acpi | grep -o "[0-9]*%" | grep -o "[0-9]*")
+    if [[ $BAT_PERCENT -le $DANGER ]]; then
+        i3-nagbar -m "Low Battery!!!" & PID=$!
+    fi
+    sleep $SLEEP"m"
+
+    if [[ -n $(acpi | grep "Discharging") ]]; then
+        echo shit
+    else
+        kill -9 $PID
+    fi
+done
