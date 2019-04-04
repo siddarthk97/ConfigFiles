347d07d818f0481a7bfe3eb5df1a74a7b2732b40
Siddarth Krishnamoorthy <siddarrth.k@gmail.com>
2019-04-05 01:13:39 +0530

    Modified wallpaper script slightly

--- src/wallpaper-script.sh
+++ src/wallpaper-script.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 #This script is to set a background for i3lock
 
-DIR=$HOME"/Pictures"
+DIR=$HOME"/Pictures/lock"
 
 if [ ! -f $DIR/wallpp.png ]
 then
-    gnome-screenshot -f $DIR/wallpp.png
-    convert -resize 10% $DIR/wallpp.png $DIR/wallpp.png
-    convert -resize 1000% $DIR/wallpp.png $DIR/wallpp.png
+    deepin-screenshot -f 
+    mv $DIR/Deep* $DIR/wallpp.png
+    #convert -resize 10% $DIR/wallpp.png $DIR/wallpp.png
+    #convert -resize 1000% $DIR/wallpp.png $DIR/wallpp.png
 else
     rm $DIR/wallpp.png
-    gnome-screenshot -f $DIR/wallpp.png
-    convert -resize 10% $DIR/wallpp.png $DIR/wallpp.png
-    convert -resize 1000% $DIR/wallpp.png $DIR/wallpp.png
+    deepin-screenshot -f 
+    mv $DIR/Deep* $DIR/wallpp.png
+    #convert -resize 10% $DIR/wallpp.png $DIR/wallpp.png
+    #convert -resize 1000% $DIR/wallpp.png $DIR/wallpp.png
 
 fi
 
